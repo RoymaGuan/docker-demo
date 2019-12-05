a5fd092523ba1e61bc78d879810ca9234ca125be
关文兵 <guanwenbing@syswin.com>
2019-12-05 15:22:49 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:8-jdk-alpine
+FROM tomcat:9.0
+MAINTAINER SeanGuan
+LABEL name="demo-image-war" version="1.0" author="SeanGuan"
+WORKDIR /usr/local/tomcat/webapps
+ADD  target/docker-demo.war .
+COPY . .
+EXPOSE 8999
+CMD ["catalina.sh","run"]
---
+++ src/main/java/com/example/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication  extends SpringBootServletInitializer {
+
+  @RequestMapping(value = "/hello-docker",method = RequestMethod.GET)
+  public String dockerHello(){
+    return "Docker Hello World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DockerDemoApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(DockerDemoApplication.class);
+  }
+}
